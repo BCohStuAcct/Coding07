c1f05fd33c5e9e785c58f6ac19d741e0b4cb8f2c
Brenda Cohen <brenda.cohen@g.austincc.edu>
2019-10-27 20:44:21 -0500

    added home page

---
+++ application/views/pages/home.php
@@ -0,0 +1,27 @@
+<!--
+Name: Brenda Cohen
+Assignment: 06
+Purpose: Home page html.
+-->
+
+<div class="body-div">
+    <div class="body-article">
+        <article>
+            <p>The most important thing to recognize about climate change is its roots in capitalism. 
+            <br>
+            Capitalism is the driving force of climate change. It's behind the push by oil companies to spend billions in order to discredit climate science. 
+            <br>
+            Any efforts to stop the climate crisis that does not recognize this will ultimately end in failure.
+            <br>
+            We no longer have time to lose.
+            <br>
+            The time to act is now.
+            </p>
+        </article>
+    </div>
+        <!-- <div class="iframe-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/h4pJSlLA_r0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div> -->
+        <div class="video-container"><iframe width="853" height="480" src='https://www.youtube.com/embed//h4pJSlLA_r0' frameborder='0' allowfullscreen></iframe></div>
+    
+</div>
