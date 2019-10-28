78cbebace4d881cecf8b30601ffeee2e8452a757
Brenda Cohen <brenda.cohen@g.austincc.edu>
2019-10-27 20:44:04 -0500

    added static pages

---
+++ application/views/pages/about.php
@@ -0,0 +1,44 @@
+<!--
+Name: Brenda Cohen
+Assignment: 06
+Purpose: About page html.
+-->
+
+<div class="body-div">
+    <div class="body-article">
+        <article>
+            <p>We are a Environmental activism group that wants to help people learn what they can do to lessen the effects of environmental destruction.</p>
+        </article>
+        <section>
+            <article class="about-item">
+                <hr>
+                <button>Shared Vision</button>
+                <div class="toggle-hide hide">
+                    <p>Our vision is to create a new world outside of capitalism that puts the planet, people, and animals first. </p>
+                </div>
+            </article>
+            <article class="about-item">
+                <hr>
+                <button>Shared Mission</button>
+                <div class="toggle-hide hide">
+                    <p>Our mission is to combat climate change by any means necessary.</p>
+                </div>
+            </article>
+            <article class="about-item">
+                <hr>
+                <button>Shared Strategy</button>
+                <div class="toggle-hide hide">
+                    <p>Our strategy incorporates many different strategies that  have led to change throughout the dawn of time. <br>We believe that utilizing many different stratgies together is the most effective and have been throughout times of social upheaval. </p>
+                </div>
+            </article>
+            <article class="about-item">
+                <hr>
+                <button>Shared Structure</button>
+                <div class="toggle-hide hide">
+                    <p>We are a horizontally organized group of autonomous individuals working collectively for the betterment of our world.<br>We are our own leaders.</p>
+                </div>
+            </article>
+        </section>
+    </div>
+</div>
+<script src="<?php echo base_url(); ?>assets/js/about.js"></script>
